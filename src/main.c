de995c11ed5bd5c313894ca0eb65bb085f973b77
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2024-02-24 20:09:04 -0800

    Sinewave enabled

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "constants.h"
 
@@ -18,11 +19,15 @@ typedef struct {
     int time;
 } Context;
 
-DEFINE_NOTE(note, 8.0, 0.25, 2048, 500);
+DEFINE_NOTE(note, 8.0, 0.25, 2048, 2000);
 Context context = {&note, 0};
 
 void soundCallback(void *buffer_data, unsigned int frames) {
     PCM_SAMPLE_TYPE *frame_data = (PCM_SAMPLE_TYPE*)buffer_data;
+
+    int true_off_period = context.note_r->off_period / context.note_r->period;
+    true_off_period += (context.note_r->off_period % context.note_r->period) != 0;
+    true_off_period *= context.note_r->period;
 
     for( unsigned int f = 0; f < frames; f++ ) {
         if(context.time % context.note_r->period > (context.note_r->period / 2)) {
@@ -31,8 +36,9 @@ void soundCallback(void *buffer_data, unsigned int frames) {
         else {
             frame_data[f] = -context.note_r->amplitude;
         }
+        frame_data[f] = (double)context.note_r->amplitude * sin(2. * M_PI * (context.time % context.note_r->period) / (double)context.note_r->period);
 
-        if(context.time > context.note_r->off_period)
+        if(context.time > true_off_period)
             frame_data[f] = 0;
 
         context.time++;
