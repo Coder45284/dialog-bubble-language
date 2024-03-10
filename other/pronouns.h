ed711170e50ed19f8d19126c479d1b02ee1b9e85
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2024-03-10 14:49:59 -0700

    Updated the pronouns table.

--- other/pronouns.h
+++ other/pronouns.h
@@ -1,118 +1,219 @@
-#define MAX_PRONOUN_PERSON 4
-#define MAX_PRONOUN_GENDER 5
-#define MAX_PRONOUN_TYPES  3
+#define MAX_PRONOUN_PERSON  4
+#define MAX_PRONOUN_GENDER  5
+#define MAX_PRONOUN_TYPES   2
+#define MAX_PRONOUN_PLURALS 2
 
-const char PRONOUN_TABLE[MAX_PRONOUN_PERSON][MAX_PRONOUN_GENDER][MAX_PRONOUN_TYPES][32] = {
+const char PRONOUN_TABLE[MAX_PRONOUN_PERSON][MAX_PRONOUN_GENDER][MAX_PRONOUN_TYPES][MAX_PRONOUN_PLURALS][32] = {
     { // 1st Person
 
         { // Unspecified
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "I/Me", // Singular
+                "We/Us" // Plural
+            },
+            { // Adjective
+                "My", // Singular
+                "Our" // Plural
+            }
         },
         { // Female
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Male
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Neuter
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Genderless
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        }
-    },
-    { // 3rd Person
-
-        { // Unspecified
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Female
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Male
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "I/Me (female)", // Singular
+                "We/Us (female)" // Plural
+            },
+            { // Adjective
+                "My (female)", // Singular
+                "Our (female)" // Plural
+            }
         },
         { // Neuter
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "I/Me (item)", // Singular
+                "We/Us (item)" // Plural
+            },
+            { // Adjective
+                "My (item)", // Singular
+                "Our (item)" // Plural
+            }
+        },
+        { // Male
+            { // Noun
+                "I/Me (male)", // Singular
+                "We/Us (male)" // Plural
+            },
+            { // Adjective
+                "My (male)", // Singular
+                "Our (male)" // Plural
+            }
         },
         { // Genderless
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "I/Me (non-binary)", // Singular
+                "We/Us (non-binary)" // Plural
+            },
+            { // Adjective
+                "My (non-binary)", // Singular
+                "Our (non-binary)" // Plural
+            }
         }
     },
     { // 1st Person Exclusionary
 
         { // Unspecified
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Female
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Male
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Neuter
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Male
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Genderless
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        }
+    },
+    { // 3rd Person
+
+        { // Unspecified
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Female
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Neuter
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Male
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Genderless
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         }
     },
     { // 2nd Person
 
         { // Unspecified
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Female
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
-        },
-        { // Male
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Neuter
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
+        },
+        { // Male
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         },
         { // Genderless
-            "BLANK", // Singular Noun
-            "BLANK", // Plural Noun
-            "BLANK"  // Adjective
+            { // Noun
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            },
+            { // Adjective
+                "BLANK", // Singular
+                "BLANK"  // Plural
+            }
         }
     }
 };
