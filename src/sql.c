1b3092023901b9eab4ba007ddc80bc07883f61e2
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2024-03-25 11:57:23 -0700

    Added an incomplete unit test for testing sql.

--- src/sql.c
+++ src/sql.c
@@ -91,14 +91,6 @@ int sqlInit(const char *const path) {
     if(db_return != SQLITE_OK)
         printf("Sqlite3 prepare error: %s\n", sqlite3_errstr(db_return));
 
-    /*
-    sqlAddWord("QeeSoeWee", "VERB",              "RUN", "The action of running.");
-    sqlAddWord("QeeSeeWel", "NOUN;ADJECTIVE",    "RED", "The color red.");
-    sqlAddWord("QoeQoeWeh", "NOUN",             "CART", "A cart like a shopping cart.");
-    sqlAddWord("ToeQeeWee", "ADVERB;ADJECTIVE", "SLOW", "Slow movement or to describe something with slow movement.");
-    sqlUpdateWord(sqlGetWordIDEnglish("CART"), "Weh", "PARTITION", "CARD", "It is non sense.");
-    */
-
     return 0;
 }
 
