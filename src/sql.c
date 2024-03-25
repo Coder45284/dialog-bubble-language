19eccb13af890e202ce4a3d79d0e141485373c6b
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2024-03-25 10:42:14 -0700

    Stoped unnessary SQL table build errors.

--- src/sql.c
+++ src/sql.c
@@ -5,17 +5,19 @@
 #include <stddef.h>
 #include <stdio.h>
 
+static int sqlLiteDoesTableExist(const char *const table_name);
 static int sqlLiteCallback(void* ignored, int argc, char** argv, char** collumn_name);
 
-sqlite3 *database = NULL;
+static sqlite3 *database = NULL;
 
-sqlite3_stmt *sql_get_language_word_to_id_code = NULL;
-sqlite3_stmt *sql_get_english_word_to_id_code = NULL;
-sqlite3_stmt *sql_get_entry_dictionary_id_code = NULL;
-sqlite3_stmt *sql_insert_dictionary_code = NULL;
-sqlite3_stmt *sql_insert_english_translation_code = NULL;
-sqlite3_stmt *sql_update_dictionary_code = NULL;
-sqlite3_stmt *sql_update_english_translation_code = NULL;
+static sqlite3_stmt *sql_check_for_table_code = NULL;
+static sqlite3_stmt *sql_get_language_word_to_id_code = NULL;
+static sqlite3_stmt *sql_get_english_word_to_id_code = NULL;
+static sqlite3_stmt *sql_get_entry_dictionary_id_code = NULL;
+static sqlite3_stmt *sql_insert_dictionary_code = NULL;
+static sqlite3_stmt *sql_insert_english_translation_code = NULL;
+static sqlite3_stmt *sql_update_dictionary_code = NULL;
+static sqlite3_stmt *sql_update_english_translation_code = NULL;
 
 int sqlInit(const char *const path) {
     int db_return;
@@ -29,28 +31,36 @@ int sqlInit(const char *const path) {
         return 1;
     }
 
-    const char SQL_BUILD_DICTIONARY[] = "CREATE TABLE DICTIONARY("
-        "W_ID "         "INTEGER PRIMARY KEY,"
-        "WORD "         "CHAR(128) UNIQUE,"
-        "PARTS_OF_SPEECH CHAR(64))";
+    db_return = sqlite3_prepare_v2(database, "SELECT * FROM sqlite_master WHERE type='table' AND tbl_name=?1", -1, &sql_check_for_table_code, NULL);
+    if(db_return != SQLITE_OK)
+        printf("Sqlite3 prepare error: %s\n", sqlite3_errstr(db_return));
 
-    db_return = sqlite3_exec(database, SQL_BUILD_DICTIONARY, sqlLiteCallback, 0, &sql_error_mesg);
+    if(!sqlLiteDoesTableExist("DICTIONARY")) {
+        const char SQL_BUILD_DICTIONARY[] = "CREATE TABLE DICTIONARY("
+            "W_ID "         "INTEGER PRIMARY KEY,"
+            "WORD "         "CHAR(128) UNIQUE,"
+            "PARTS_OF_SPEECH CHAR(64))";
 
-    if(db_return != SQLITE_OK) {
-        printf("Sqlite3 error: %s\n", sql_error_mesg);
-        sqlite3_free(sql_error_mesg);
+        db_return = sqlite3_exec(database, SQL_BUILD_DICTIONARY, sqlLiteCallback, 0, &sql_error_mesg);
+
+        if(db_return != SQLITE_OK) {
+            printf("Sqlite3 error: %s\n", sql_error_mesg);
+            sqlite3_free(sql_error_mesg);
+        }
     }
 
-    const char SQL_BUILD_ENGLISH_TRANSLATION[] = "CREATE TABLE ENGLISH_TRANSLATION("
-        "W_ID "    "INT PRIMARY KEY NOT NULL,"
-        "KEYWORD " "CHAR(64),"
-        "DEFINITION TEXT)";
+    if(!sqlLiteDoesTableExist("ENGLISH_TRANSLATION")) {
+        const char SQL_BUILD_ENGLISH_TRANSLATION[] = "CREATE TABLE ENGLISH_TRANSLATION("
+            "W_ID "    "INT PRIMARY KEY NOT NULL,"
+            "KEYWORD " "CHAR(64),"
+            "DEFINITION TEXT)";
 
-    db_return = sqlite3_exec(database, SQL_BUILD_ENGLISH_TRANSLATION, sqlLiteCallback, 0, &sql_error_mesg);
+        db_return = sqlite3_exec(database, SQL_BUILD_ENGLISH_TRANSLATION, sqlLiteCallback, 0, &sql_error_mesg);
 
-    if(db_return != SQLITE_OK) {
-        printf("Sqlite3 error: %s\n", sql_error_mesg);
-        sqlite3_free(sql_error_mesg);
+        if(db_return != SQLITE_OK) {
+            printf("Sqlite3 error: %s\n", sql_error_mesg);
+            sqlite3_free(sql_error_mesg);
+        }
     }
 
     db_return = sqlite3_prepare_v2(database, "SELECT W_ID FROM DICTIONARY WHERE WORD=?1;", -1, &sql_get_language_word_to_id_code, NULL);
@@ -94,6 +104,7 @@ int sqlInit(const char *const path) {
 
 void sqlDeinit() {
 
+    sqlite3_finalize(sql_check_for_table_code);
     sqlite3_finalize(sql_get_language_word_to_id_code);
     sqlite3_finalize(sql_get_english_word_to_id_code);
     sqlite3_finalize(sql_get_entry_dictionary_id_code);
@@ -142,7 +153,7 @@ int sqlGetWordIDEnglish(const char *const word) {
     sqlite3_int64 id_number = 0;
 
     if(sql_get_english_word_to_id_code != NULL) {
-        sqlite3_bind_text(sql_get_english_word_to_id_code, 1, word, -1, NULL);\
+        sqlite3_bind_text(sql_get_english_word_to_id_code, 1, word, -1, NULL);
 
         db_return = sqlite3_step(sql_get_english_word_to_id_code);
 
@@ -288,6 +299,38 @@ int sqlUpdateWord(int word_id, const char *const word, const char *const parts_o
     }
 
     return word_id;
+}
+
+static int sqlLiteDoesTableExist(const char *const table_name) {
+    int db_return;
+    int found = 0; // False
+
+    if(sql_check_for_table_code != NULL) {
+        sqlite3_bind_text(sql_check_for_table_code, 1, table_name, -1, NULL);
+
+        db_return = sqlite3_step(sql_check_for_table_code);
+
+        for(int limit = 0; limit < 256 && db_return == SQLITE_BUSY; limit++) {
+            db_return = sqlite3_step(sql_check_for_table_code);
+        }
+
+        if(db_return == SQLITE_ROW)
+            found = 1; // True
+
+        db_return = sqlite3_step(sql_check_for_table_code);
+
+        for(int limit = 0; limit < 256 && db_return == SQLITE_BUSY; limit++) {
+            db_return = sqlite3_step(sql_check_for_table_code);
+        }
+
+        if(db_return != SQLITE_DONE) {
+            printf("sqlLiteDoesTableExist prepare error: %s\n", sqlite3_errstr(db_return) );
+        }
+
+        sqlite3_reset(sql_check_for_table_code);
+    }
+
+    return found;
 }
 
 static int sqlLiteCallback(void* ignored, int argc, char** argv, char** collumn_name) {
