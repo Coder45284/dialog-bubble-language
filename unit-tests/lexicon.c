7714d9dd8778e2835c6b420b7ed369f99b7e1d73
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2024-04-05 22:09:56 -0700

    This unit test detected a problem with number eight

--- unit-tests/lexicon.c
+++ unit-tests/lexicon.c
@@ -1,13 +1,42 @@
 #include "grammer.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void lexerTestCallback(int token_type, const YYSTYPE *const yystype);
 
 const char *const tokenTypeString(int token_type);
 
-static const char text[] = "SieSoe SieSoeToe SieWoe SieWoeToe SieSelSoe SieSelSoeToe SieSelWoe SieSelWoeToe SieWelSoe SieWelSoeToe SieWelWoe SieWelWoeToe SieTelSoe SieTelSoeToe SieTelWoe SieTelWoeToe SieQelSoe SieQelSoeToe SieQelWoe SieQelWoeToe WieSoe WieSoeToe WieWoe WieWoeToe WieSelSoe WieSelSoeToe WieSelWoe WieSelWoeToe WieWelSoe WieWelSoeToe WieWelWoe WieWelWoeToe WieTelSoe WieTelSoeToe WieTelWoe WieTelWoeToe WieQelSoe WieQelSoeToe WieQelWoe WieQelWoeToe TieSoe TieSoeToe TieWoe TieWoeToe TieSelSoe TieSelSoeToe TieSelWoe TieSelWoeToe TieWelSoe TieWelSoeToe TieWelWoe TieWelWoeToe TieTelSoe TieTelSoeToe TieTelWoe TieTelWoeToe TieQelSoe TieQelSoeToe TieQelWoe TieQelWoeToe QieSoe QieSoeToe QieWoe QieWoeToe QieSelSoe QieSelSoeToe QieSelWoe QieSelWoeToe QieWelSoe QieWelSoeToe QieWelWoe QieWelWoeToe QieTelSoe QieTelSoeToe QieTelWoe QieTelWoeToe QieQelSoe QieQelSoeToe QieQelWoe QieQelWoeToe Seh Sel Tel Weh Qel Qeh Tie Teh Tee Toe Sie Soe See Qee SelSel TelSeh TeeWel QeeTelQee TelQeeTel QehTel QelSee WelQeeQee SehWee SeeSee SehTee QehTee QehSehTee TehTee TehSehTee TehQehTee TehQehSehTee WehTee WehSehTee WehQehTee WehQehSehTee WehTehTee WehTehSehTee WehTehQehTee WehTehQehSehTee SeeTee QeeTee QeeSeeTee TeeTee TeeSeeTee TeeQeeTee TeeQeeSeeTee WeeTee WeeSeeTee WeeQeeTee WeeQeeSeeTee WeeTeeTee WeeTeeSeeTee WeeTeeQeeTee WeeTeeQeeSeeTee SelTee QelTee QelSelTee TelTee TelSelTee TelQelTee TelQelSelTee WelTee WelSelTee WelQelTee WelQelSelTee WelTelTee WelTelSelTee WelTelQelTee WelTelQelSelTee SeeTeh QeeTeh QeeSeeTeh TeeTeh TeeSeeTeh TeeQeeTeh TeeQeeSeeTeh WeeTeh WeeSeeTeh WeeQeeTeh WeeQeeSeeTeh WeeTeeTeh WeeTeeSeeTeh WeeTeeQeeTeh WeeTeeQeeSeeTeh SeeTel QeeTel QeeSeeTel TeeTel TeeSeeTel TeeQeeTel TeeQeeSeeTel WeeTel WeeSeeTel WeeQeeTel WeeQeeSeeTel WeeTeeTel WeeTeeSeeTel WeeTeeQeeTel WeeTeeQeeSeeTel TeeWelTee SeeWehTee QieQelQoeSeeQel SehSeeQel SelSeeQel TehSeeQel WehSeeQel QieQelQoeSeeQolTeh SehSeeQolTeh SelSeeQolTeh TehSeeQolTeh WehSeeQolTeh QieQelQoeSeeTel SehSeeTel SelSeeTel TehSeeTel WehSeeTel QieQelQoeSeeWeh SehSeeWeh SelSeeWeh TehSeeWeh WehSeeWeh QieQelQoeSeeWoe SehSeeWoe SelSeeWoe TehSeeWoe WehSeeWoe ";
-static const int token_types[] = {NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, CONJUNCTION, CONJUNCTION, CONJUNCTION, CONJUNCTION, PHRASE_CONJUNCTION, PHRASE_CONJUNCTION, IF, ELIF, ELSE, THEN, GROUP_BEGIN, GROUP_END, DELIMITER, DELIMITER, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_SIGN, NUMBER_SIGN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, };
+static const char text[] = "SieSoe SieSoeToe SieWoe SieWoeToe SieSelSoe SieSelSoeToe SieSelWoe SieSelWoeToe SieWelSoe SieWelSoeToe SieWelWoe SieWelWoeToe SieTelSoe SieTelSoeToe SieTelWoe SieTelWoeToe SieQelSoe SieQelSoeToe SieQelWoe SieQelWoeToe WieSoe WieSoeToe WieWoe WieWoeToe WieSelSoe WieSelSoeToe WieSelWoe WieSelWoeToe WieWelSoe WieWelSoeToe WieWelWoe WieWelWoeToe WieTelSoe WieTelSoeToe WieTelWoe WieTelWoeToe WieQelSoe WieQelSoeToe WieQelWoe WieQelWoeToe TieSoe TieSoeToe TieWoe TieWoeToe TieSelSoe TieSelSoeToe TieSelWoe TieSelWoeToe TieWelSoe TieWelSoeToe TieWelWoe TieWelWoeToe TieTelSoe TieTelSoeToe TieTelWoe TieTelWoeToe TieQelSoe TieQelSoeToe TieQelWoe TieQelWoeToe QieSoe QieSoeToe QieWoe QieWoeToe QieSelSoe QieSelSoeToe QieSelWoe QieSelWoeToe QieWelSoe QieWelSoeToe QieWelWoe QieWelWoeToe QieTelSoe QieTelSoeToe QieTelWoe QieTelWoeToe QieQelSoe QieQelSoeToe QieQelWoe QieQelWoeToe Seh Sel Tel Weh Qel Qeh Tie Teh Tee Toe Sie Soe See Qee SelSel TelSeh TeeWel QeeTelQee TelQeeTel QehTel QelSee WelQeeQee SehWee SeeSee SehTee QehTee QehSehTee TehTee TehSehTee TehQehTee TehQehSehTee WehTee WehSehTee WehQehTee WehQehSehTee WehTehTee WehTehSehTee WehTehQehTee WehTehQehSehTee SelTee QelTee QelSelTee TelTee TelSelTee TelQelTee TelQelSelTee WelTee WelSelTee WelQelTee WelQelSelTee WelTelTee WelTelSelTee WelTelQelTee WelTelQelSelTee SeeTee QeeTee QeeSeeTee TeeTee TeeSeeTee TeeQeeTee TeeQeeSeeTee WeeTee WeeSeeTee WeeQeeTee WeeQeeSeeTee WeeTeeTee WeeTeeSeeTee WeeTeeQeeTee WeeTeeQeeSeeTee SeeTel QeeTel QeeSeeTel TeeTel TeeSeeTel TeeQeeTel TeeQeeSeeTel WeeTel WeeSeeTel WeeQeeTel WeeQeeSeeTel WeeTeeTel WeeTeeSeeTel WeeTeeQeeTel WeeTeeQeeSeeTel SeeTeh QeeTeh QeeSeeTeh TeeTeh TeeSeeTeh TeeQeeTeh TeeQeeSeeTeh WeeTeh WeeSeeTeh WeeQeeTeh WeeQeeSeeTeh WeeTeeTeh WeeTeeSeeTeh WeeTeeQeeTeh WeeTeeQeeSeeTeh TeeWelTee SeeWehTee QieQelQoeSeeQel SehSeeQel SelSeeQel TehSeeQel WehSeeQel QieQelQoeSeeQolTeh SehSeeQolTeh SelSeeQolTeh TehSeeQolTeh WehSeeQolTeh QieQelQoeSeeTel SehSeeTel SelSeeTel TehSeeTel WehSeeTel QieQelQoeSeeWeh SehSeeWeh SelSeeWeh TehSeeWeh WehSeeWeh QieQelQoeSeeWoe SehSeeWoe SelSeeWoe TehSeeWoe WehSeeWoe";
+
+static const int token_types[] = {NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, NOUN, NOUN, ADJECTIVE, ADJECTIVE, CONJUNCTION, CONJUNCTION, CONJUNCTION, CONJUNCTION, PHRASE_CONJUNCTION, PHRASE_CONJUNCTION, IF, ELIF, ELSE, THEN, GROUP_BEGIN, GROUP_END, DELIMITER, DELIMITER, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, PROPOSITION, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_100, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_10, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_1, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_PLACE, NUMBER_SIGN, NUMBER_SIGN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN, NOUN};
+
+static const char *return_text[] = {
+    "I/Me", "We/Us", "My", "Our",
+    "I/Me (female)", "We/Us (female)", "My (female)", "Our (female)",
+    "I/Me (neuter)", "We/Us (neuter)", "My (neuter)", "Our (neuter)",
+    "I/Me (male)", "We/Us (male)", "My (male)", "Our (male)",
+    "I/Me (non-binary)", "We/Us (non-binary)", "My (non-binary)", "Our (non-binary)",
+    "I/Me, not you", "We/Us, not you", "My, not you", "Our, not you",
+    "I/Me (female), not you", "We/Us (female), not you", "My (female), not you", "Our (female), not you",
+    "I/Me (neuter), not you", "We/Us (neuter), not you", "My (neuter), not you", "Our (neuter), not you",
+    "I/Me (male), not you", "We/Us (male), not you", "My (male), not you", "Our (male), not you",
+    "I/Me (non-binary), not you", "We/Us (non-binary), not you", "My (non-binary), not you", "Our (non-binary), not you",
+    "They (singular)", "They", "Their (singular)", "Their",
+    "She", "They (all-female)", "Her", "Their (all-female)",
+    "It", "They (all-neuter)", "Its", "Their (all-neuter)",
+    "He", "They (all-male)", "His", "Their (all-male)",
+    "They (singular,all-non-binary)", "They (plural,all-non-binary)", "Their (singular,all-non-binary)", "Their (plural,all-non-binary)",
+    "You (singular)", "You all (plural)", "Your (singular)", "You all's (plural)",
+    "You (female,singular)", "You all (female,plural)", "Your (female,singular)", "You all's (female,plural)",
+    "You (neuter,singular)", "You all (neuter,plural)", "Your (neuter,singular)", "You all's (neuter,plural)",
+    "You (male,singular)", "You all (male,plural)", "Your (male,singular)", "You all's (male,plural)",
+    "You (non-binary,singular)", "You all (non-binary,plural)", "Your (non-binary,singular)", "You all's (non-binary,plural)",
+    "And", "Or", "Because", "So", "And", "Or", "", "", "", "", "", "", "END", "END Yes|No", "Similar to", "Because", "Using", "To", "From", "On/At", "In/Inside of", "For", "Because of", "Of",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "1", "-1",
+    "Nowhere", "Everywhere", "Somewhere", "Where", "There", "None (no amount)", "All (amount)", "Some (amount)", "How much", "That amount", "No reason", "Every reason", "Some reason", "Why", "That reason", "Never", "Everytime", "Sometime", "When", "Then", "No one/Nothing", "Everyone/Everything", "Someone/Something", "Who/What", "That/That (one)"};
+
+
 int token_type_index = 0;
 int problem = 0;
 
@@ -22,6 +51,8 @@ int main() {
 extern char *yytext;
 
 void lexerTestCallback(int token_type, const YYSTYPE *const yystype) {
+    char result[sizeof(YYSTYPE)];
+
     if(problem)
         return;
 
@@ -34,6 +65,7 @@ void lexerTestCallback(int token_type, const YYSTYPE *const yystype) {
         case ELSE:
         case THEN: {
             printf("%s(%d)\n", tokenTypeString(token_type), token_type);
+            result[0] = '\0';
             break;
         }
         case DELIMITER:
@@ -47,6 +79,9 @@ void lexerTestCallback(int token_type, const YYSTYPE *const yystype) {
         case NOT_A_KEYWORD:
         {
             printf("%s(%d) = \"%s\"\n", tokenTypeString(token_type), token_type, yystype->word);
+
+            snprintf(result, sizeof(result) / sizeof(result[0]), "%s", yystype->word);
+
             break;
         }
         case NUMBER_SIGN:
@@ -55,20 +90,32 @@ void lexerTestCallback(int token_type, const YYSTYPE *const yystype) {
         case NUMBER_1:
         {
             printf("%s(%d) = %lli\n", tokenTypeString(token_type), token_type, yystype->number);
+
+            snprintf(result, sizeof(result) / sizeof(result[0]), "%lli", yystype->number);
+
             break;
         }
         case NUMBER_PLACE:
         {
             printf("%s(%d) = %Lf\n", tokenTypeString(token_type), token_type, yystype->real_number);
+
+            snprintf(result, sizeof(result) / sizeof(result[0]), "%Lf", yystype->real_number);
+
             break;
         }
         default: {
             printf("%s(%d) = %s\n", tokenTypeString(token_type), token_type, yystype->word);
+
+            snprintf(result, sizeof(result) / sizeof(result[0]), "%s", "N/A");
         }
     }
 
     if(token_types[token_type_index] != token_type) {
         printf("Type wrong expected %s(%d) for word at %i index. %s\n", tokenTypeString(token_types[token_type_index]), token_types[token_type_index], token_type_index, yytext);
+        problem = 1;
+    }
+    if(strcmp(return_text[token_type_index], result) != 0) {
+        printf("Return does not match expected %s(%d) for word at %i index. \"%s\" is not \"%s\"\n", tokenTypeString(token_types[token_type_index]), token_types[token_type_index], token_type_index, return_text[token_type_index], result);
         problem = 1;
     }
     token_type_index++;
